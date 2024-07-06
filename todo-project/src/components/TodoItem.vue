036c5101b2b0392b269bc656383ee6512169307c
AmabilisH <amabilis.jesus.andres98@gmail.com>
2024-07-06 08:23:40 -0400

    Add files via upload

--- todo-project/src/components/TodoItem.vue
+++ todo-project/src/components/TodoItem.vue
@@ -1,38 +1,41 @@
 <template>
-    <el-col :span="12">
-      <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-        <el-row :gutter="12">
-          <el-col :span="21">{{ todo }}</el-col>
-          <el-col :span="3">
-            <el-button @click="remove" type="success" icon="el-icon-check" circle></el-button>
-          </el-col>
-        </el-row>
-      </el-card>
-    </el-col>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TodoItem',
-    props: {
-      todo: {
-        type: String,
-        required: true
-      },
-      index: {
-        type: Number,
-        required: true
-      }
+  <el-col :span="12">
+    <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+      <el-row :gutter="12">
+        <el-col :span="21">{{ item }}</el-col>
+        <el-col :span="3">
+          <el-button @click="removeItem(index)" type="success" icon="el-icon-check" circle></el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </el-col>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    item: {
+      type: String,
+      required: true
     },
-    methods: {
-      remove() {
-        this.$emit('removeTodo', this.index);
-      }
+    index: {
+      type: Number,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    removeItem(index) {
+      this.$emit('remove-item', index);
     }
   }
-  </script>
-  
-  <style scoped>
-  /* Añade estilos específicos para este componente aquí */
-  </style>
-  
+};
+</script>
+
+<style scoped>
+
+</style>
--- todo-project/src/components/TodosIssues.vue
+++ todo-project/src/components/TodosIssues.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Lista de tareas</h1>
+    <h1>Lista de tareas y problemas</h1>
     <!-- formulario de entrada de tareas -->
     <form @submit.prevent="addTodo">
       <el-input placeholder="Agregar nueva tarea" v-model="todo"></el-input>
@@ -10,22 +10,21 @@
       <!-- área de visualización de tareas pendientes -->
       <TodoItem
         v-for="(todo, index) in todos"
-        :key="index"
-        :todo="todo"
+        :key="`todo-${index}`"
+        :item="todo"
         :index="index"
-        @removeTodo="removeTodo"
+        @remove-item="removeTodo"
+        type="todo"
       />
       <!-- zona de visualización de problemas -->
-      <el-col :span="12" v-for="(issue, index) in issues" :key="issue.id">
-        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-          <el-row :gutter="12">
-            <el-col :span="21">{{ issue.title }}</el-col>
-            <el-col :span="3">
-              <el-button @click="closeIssue(index)" type="success" icon="el-icon-check" circle></el-button>
-            </el-col>
-          </el-row>
-        </el-card>
-      </el-col>
+      <TodoItem
+        v-for="(issue, index) in issues"
+        :key="`issue-${index}`"
+        :item="issue.title"
+        :index="index"
+        @remove-item="closeIssue"
+        type="issue"
+      />
     </el-row>
   </div>
 </template>
@@ -48,47 +47,43 @@ export default {
   components: {
     TodoItem
   },
-  data () {
+  data() {
     return {
       todo: '',
       todos: [],
       issues: []
-    }
+    };
   },
   methods: {
-    addTodo(){
+    addTodo() {
       if (this.todo.trim() !== '') {
         this.todos.push(this.todo);
         this.todo = '';
       }
     },
-    removeTodo(index){
+    removeTodo(index) {
       this.todos.splice(index, 1);
     },
-    closeIssue(index){
+    closeIssue(index) {
       const target = this.issues[index];
-      client.patch(`/issues/${target.number}`,
-          {
-            state: 'closed'
-          },
-        )
-        .then(() => {
-         this.issues.splice(index, 1);
-      })
+      client.patch(`/issues/${target.number}`, {
+        state: 'closed'
+      }).then(() => {
+        this.issues.splice(index, 1);
+      });
     },
     getIssues() {
-      client.get('issues')
-        .then((res) => {
-          this.issues = res.data
-      })
+      client.get('issues').then((res) => {
+        this.issues = res.data;
+      });
     }
   },
   created() {
     this.getIssues();
   }
-}
+};
 </script>
 
 <style scoped>
-/* Añade estilos específicos para este componente aquí */
+
 </style>
